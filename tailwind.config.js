f2e09b511c08ebe0616af5505b744cffeb50f5cb
Mcmxcvi25 <tema.polivoda@gmail.com>
2024-10-16 18:05:53 +0300

    16/10/24 - update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,18 @@
 module.exports = {
 	content: ['./**/*.html'],
 	theme: {
+		screens: {
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px',
+		},
 		extend: {
 			backgroundImage: {
 				'chair-img': "url('../img/main-chair.png')",
+			},
+			screens: {
+				sm: '480px',
 			},
 			fontFamily: {
 				sans: ['Inter'],
@@ -13,13 +22,6 @@ module.exports = {
 			},
 			container: {
 				center: true,
-				screens: {
-					xs: '320px',
-					sm: '480px',
-					md: '768px',
-					lg: '1024px',
-					xl: '1440px',
-				},
 			},
 		},
 	},
